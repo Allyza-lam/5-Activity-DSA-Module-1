15d75237c00346b6b4895e719a5641c9a4a063db
Allyza-lam <allyzanantuna56@gmail.com>
2024-09-13 22:15:51 +0800

    Create Strings.js

---
+++ String.js
@@ -0,0 +1,20 @@
+//Create a variable named favActorFirstName and store your favorite actor's first name
+let favActorFirstName = "Lovie";
+//Create a variable named favActorLastName and store their last name
+let favActorLastName = "mayyang";
+let fullName = favActorFirstName + "" + favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+
+let lowercase = "yayot".toUpperCase();
+
+let message = "My favorite actor is " + fullName.toUpperCase();
+
+message += ",his best show is Silicon Valley.";
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message);
